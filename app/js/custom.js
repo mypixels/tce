f604c269f37a19417b009e08570205aee50639a2
Matt <matt_live@live.co.uk>
2017-08-16 23:06:48 +0100

    gallery

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#menu-icon').on('click', function() {
-    $('.navbar').toggleClass('expand');
+    $('.main-navbar').toggleClass('expand');
     return false;
   })
 });
